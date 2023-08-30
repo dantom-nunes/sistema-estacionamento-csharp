44a4193c3a49ca78670416b8fb4872bf79956213
dantom-nunes <dantom.nunes@hotmail.com>
2023-08-29 21:09:52 -0300

    Exemplo Curso de Ingles

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,22 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa("Dantom", "Nunes");
+Pessoa p2 = new Pessoa("Eduardo", "Neves");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+// p1.Idade = 24;
+// p1.Apresentar ();
